ee2d0c49979d025362d16c1d2e80e21c44c7880e
Nikola Bodrozic <nikola@example.com>
2017-05-31 12:01:29 +0200

    code is formated

--- ajax.php
+++ ajax.php
@@ -9,22 +9,24 @@ $dataBase = "test";
 $character_set = "utf8";
 $conn = new PDO("mysql:host=$hostname;dbname=$dataBase;charset=$character_set", $username, $password);
 
-//usleep(250000);
-if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-	$headers = apache_request_headers();
-	$flag = false;
-	
-	foreach ($headers as $header => $value) {
-		if($header == "X-Sec-Header" && $value == $_SESSION["sectok"]) $flag = true;
-	}
-	
-	if($flag) {
-    //var_dump($_POST);die;
-    $minprice = $_POST["minprice"]; //5000;
-    $maxprice = $_POST["maxprice"]; //11000;
-    try {
-     $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-     $stmt = $conn -> prepare("SELECT 
+// is AJAX call? check the HTTP header
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $headers = apache_request_headers();
+    $flag = false;
+
+    // check for my HTTP header
+    foreach ($headers as $header => $value) {
+        if ($header == "X-Sec-Header" && $value == $_SESSION["sectok"])
+            $flag = true;
+    }
+
+    // start processing POSTed data
+    if ($flag) {
+        $minprice = $_POST["minprice"]; //5000;
+        $maxprice = $_POST["maxprice"]; //11000;
+        try {
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare("SELECT 
       a.id, 
       rent_amount, 
       slug,
@@ -37,27 +39,29 @@ if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQU
         AND 
       rent_amount BETWEEN :minprice AND :maxprice");
 
-     $stmt->bindParam(":id", $_POST['id'], PDO::PARAM_INT);
-     $stmt->bindParam(":minprice", $minprice, PDO::PARAM_INT);
-     $stmt->bindParam(":maxprice", $maxprice, PDO::PARAM_INT);     
-     $stmt->execute();
+            $stmt->bindParam(":id", $_POST['id'], PDO::PARAM_INT);
+            $stmt->bindParam(":minprice", $minprice, PDO::PARAM_INT);
+            $stmt->bindParam(":maxprice", $maxprice, PDO::PARAM_INT);
+            $stmt->execute();
 
-     $arr = array();
-     array_push($arr, array("numrows" => $stmt->rowCount()) );
-     while ($row = $stmt -> fetch()) {
-        array_push($arr, array(
-          "rent" => $row['rent_amount'], 
-          "slug" => $row['slug'], 
-          "img" => "house".$row['id'].".jpg",
-          "lat" => $row['lat'],
-          "lang" => $row['lang']
-        ));
-     }
-     echo json_encode($arr);
+            // create array
+            $arr = array();
+            array_push($arr, array("numrows" => $stmt->rowCount()));
+            while ($row = $stmt->fetch()) {
+                array_push($arr, array(
+                    "rent" => $row['rent_amount'],
+                    "slug" => $row['slug'],
+                    "img" => "house" . $row['id'] . ".jpg",
+                    "lat" => $row['lat'],
+                    "lang" => $row['lang']
+                ));
+            }
 
-    } catch(PDOException $e) {
-     echo 'ERROR: ' . $e -> getMessage();
-     die;
-    }    
-	}
+            // create and send JSON based on the array
+            echo json_encode($arr);
+        } catch (PDOException $e) {
+            echo 'ERROR: ' . $e->getMessage();
+            die;
+        }
+    }
 }
--- index.php
+++ index.php
@@ -5,209 +5,214 @@ $_SESSION["sectok"] = uniqid(true);
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Apartment finder</title>
-    <!-- Bootstrap and demo CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Apartment finder</title>
+        <!-- Bootstrap and demo CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet">
 
-    <!-- select2 CSS -->
-    <link href="css/select2.min.css" rel="stylesheet" />
+        <!-- select2 CSS -->
+        <link href="css/select2.min.css" rel="stylesheet" />
 
-    <!-- slider CSS -->
-    <link href="css/slider.min.css" rel="stylesheet">
+        <!-- slider CSS -->
+        <link href="css/slider.min.css" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--if lt IE 9
-        script(src='https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js')
-        script(src='https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js')
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--if lt IE 9
+            script(src='https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js')
+            script(src='https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js')
         -->
-</head>
+    </head>
 
-<body>
-    <!-- Fixed navbar -->
-    <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span> <span
-                        class="icon-bar"></span> <span class="icon-bar"></span> <span
-                        class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Place for logo</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-            </div>
-            <!--/.nav-collapse -->
-        </div>
-    </nav>
-
-    <div class="container" style="margin-top: 110px">
-        <div class="row">
-            <div class="col-md-4 hidden-xs">
-                    <h3>Filter</h3>
-                <div class="filter-holder">
-                    <div>Filter by price in euros</div>
-                    <div>
-                        <b>10000 &nbsp;&nbsp;</b>
-                        <input type="text" class="span2" value="" data-slider-min="10000" data-slider-max="50000" data-slider-step="1000" data-slider-value="[10000,50000]" id="housePrice">
-                        <b> &nbsp;&nbsp; 50000</b>
-                    </div>
+    <body>
+        <!-- Fixed navbar -->
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span> <span
+                            class="icon-bar"></span> <span class="icon-bar"></span> <span
+                            class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Place for logo</a>
                 </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                </div>
+                <!--/.nav-collapse -->
+            </div>
+        </nav>
 
-                <br>        
+        <div class="container" style="margin-top: 110px">
+            <div class="row">
+                <!-- sidebar  -->
+                <div class="col-md-4 hidden-xs">
+                    <h3>Filter</h3>
+                    <!-- range slider  -->
+                    <div class="filter-holder">
+                        <div>Filter by price in euros</div>
+                        <div>
+                            <b>10000 &nbsp;&nbsp;</b>
+                            <input type="text" class="span2" value="" data-slider-min="10000" data-slider-max="50000" data-slider-step="1000" data-slider-value="[10000,50000]" id="housePrice">
+                            <b> &nbsp;&nbsp; 50000</b>
+                        </div>
+                    </div>
 
-                <div class="filter-holder">
+                    <br>        
+                    <!-- select2 element  -->
+                    <div class="filter-holder">
+
+                        <hr>
+                        <div>Area</div>
+                        <div>
+                            <select class="area-val" style="width: 100%">
+                                <option value="1">Hillvesum</option>
+                                <option value="2">Slagelse</option>
+                                <option value="3">Hellerup</option>          
+                            </select>
+                        </div>
+                    </div>
+
+
+                </div>
+                <!-- main content  -->
+                <div class="col-md-8">
+                    <h3>Results</h3>
+                    <hr>
+                    <div id="out" style="height:50px"></div>
+                    <div id="platno" style="width: 516px; height: 250px"><img id="imgload" src="images/ajax-loader.gif" /></div>
 
                     <hr>
-                    <div>Area</div>
-                    <div>
-                        <select class="area-val" style="width: 100%">
-                           <option value="1">Hillvesum</option>
-                           <option value="2">Slagelse</option>
-                           <option value="3">Hellerup</option>          
-                  </select>
-                    </div>
+
+                    <div id="loader"> </div>
                 </div>
-
-
-            </div>
-            <div class="col-md-8">
-                <h3>Results</h3>
-                <hr>
-                <div id="out" style="height:50px"></div>
-    <div id="platno" style="width: 516px; height: 250px"><img id="imgload" src="images/ajax-loader.gif" /></div>
-               
-                <hr>
-
-                <div id="loader"> </div>
             </div>
         </div>
-    </div>
 
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+        <!-- Bootstrap core JavaScript-->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
 
-    <!-- select2 JavaScript-->
-    <script src="js/select2.min.js"></script>
+        <!-- select2 JavaScript-->
+        <script src="js/select2.min.js"></script>
 
-    <!-- slider JavaScript-->
-    <script src="js/bootstrap-slider.min.js"></script>
-    
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC8ytq0lf_FAeZg9MKJwockhO73p1J93co"></script>                
+        <!-- slider JavaScript-->
+        <script src="js/bootstrap-slider.min.js"></script>
 
-    <script type="text/javascript">
-        var min;
-        var max;
-        var id;
-        var lat = Array();
-        var lang = Array();
-        
+        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC8ytq0lf_FAeZg9MKJwockhO73p1J93co"></script>                
+
+        <script type="text/javascript">
+            // initial parameters
+            var min;
+            var max;
+            var id;
+            var lat = Array();
+            var lang = Array();
+
+            // iniialise range slider
             $('#housePrice').slider({
                 tooltip: 'show'
             });
 
+            // initialise select2 element
             $(".area-val").select2();
 
-        $(document).ready(function() {
-
-
-
-            min = $('#housePrice').data("slider-min");
-            max = $('#housePrice').data("slider-max");
-
-            makeAjax(min, max);
-        });
-
-        $('.area-val').on('select2:select',
-            function(evt) {
+            $(document).ready(function () {
+                min = $('#housePrice').data("slider-min");
+                max = $('#housePrice').data("slider-max");
                 makeAjax(min, max);
             });
 
-        $("#housePrice").on("slideStop", function(slideEvt) {
-            var t = slideEvt.value;
-            min = t[0];
-            max = t[1];
-            makeAjax(t[0], t[1]);
-        });
+            // handle select event on select2 element
+            $('.area-val').on('select2:select',
+                    function (evt) {
+                        makeAjax(min, max);
+                    });
 
-        function makeAjax(min, max) {
-            var host = location.protocol + '//' + location.hostname;
-
-            var request = $.ajax({
-                url: host + '/folkmatic/ajax.php',
-                method: "POST",
-                data: {
-                    id: $('.area-val').val(),
-                    minprice: min,
-                    maxprice: max
-                },
-                beforeSend: function(xhr) {
-                   // $("#out").hide();
-                    //$("#platno").append('<img id="imgload" src="images/ajax-loader.gif" />');
-                    xhr.setRequestHeader('X-Sec-Header', '<?php echo $_SESSION["sectok"]; ?>' );
-                }
+            // handle slide stop event
+            $("#housePrice").on("slideStop", function (slideEvt) {
+                var t = slideEvt.value;
+                min = t[0];
+                max = t[1];
+                makeAjax(t[0], t[1]);
             });
 
-            request.done(function(msg) {
+            // fetch JSON from backend
+            function makeAjax(min, max) {
+                var host = location.protocol + '//' + location.hostname;
 
-                var obj = jQuery.parseJSON(msg);
-                var nr = obj[0].numrows;
-                lat = [];   
-                lang = [];
-               locations = [];
-               for (i = 1; i <= nr; i++) {
-                   locations.push([obj[i].slug, obj[i].lat, obj[i].lang], i);
-               }
-                afterLoad(nr, locations);      
-               //$("#out").show();
-                $("#out").html("Query found " + nr + " result(s)");//.fadeIn("slow", "linear");
-            });
+                var request = $.ajax({
+                    url: host + '/folkmatic/ajax.php',
+                    method: "POST",
+                    data: {
+                        id: $('.area-val').val(),
+                        minprice: min,
+                        maxprice: max
+                    },
+                    beforeSend: function (xhr) {
+                        // $("#out").hide();
+                        //$("#platno").append('<img id="imgload" src="images/ajax-loader.gif" />');
+                        xhr.setRequestHeader('X-Sec-Header', '<?php echo $_SESSION["sectok"]; ?>');
+                    }
+                });
 
-            request.fail(function(jqXHR, textStatus) {
-                alert("Request failed: " + textStatus);
-            });
+                request.done(function (msg) {
+                    var obj = jQuery.parseJSON(msg);
+                    var nr = obj[0].numrows;
+                    lat = [];
+                    lang = [];
+                    locations = [];
+                    for (i = 1; i <= nr; i++) {
+                        locations.push([obj[i].slug, obj[i].lat, obj[i].lang], i);
+                    }
+                    afterLoad(nr, locations);
+                    //$("#out").show();
+                    $("#out").html("Query found " + nr + " result(s)");//.fadeIn("slow", "linear");
+                });
 
-            request.always(function() {
-                //$("#imgload").remove();
-            });
-        }
+                request.fail(function (jqXHR, textStatus) {
+                    alert("Request failed: " + textStatus);
+                });
 
-       function afterLoad(nr, locations) {
-           var mapDiv = document.getElementById('platno');
-           var map = new google.maps.Map(mapDiv, {
-               center: new google.maps.LatLng(55.399833, 11.374667),
-               zoom: 6,
-               disableDefaultUI: false,
-               mapTypeId: google.maps.MapTypeId.ROADMAP,
-               scrollwheel: false,
-               navigationControl: true
-           });
+                request.always(function () {
+                    //$("#imgload").remove();
+                });
+            }
 
-           var infowindow = new google.maps.InfoWindow();
+            // render pins on google map
+            function afterLoad(nr, locations) {
+                var mapDiv = document.getElementById('platno');
+                var map = new google.maps.Map(mapDiv, {
+                    center: new google.maps.LatLng(55.399833, 11.374667),
+                    zoom: 6,
+                    disableDefaultUI: false,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP,
+                    scrollwheel: false,
+                    navigationControl: true
+                });
 
-           var marker, i;
+                var infowindow = new google.maps.InfoWindow();
 
-           for (i = 0; i < locations.length; i++) {
-               marker = new google.maps.Marker({
-                   position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-                   map: map
-               });
+                var marker, i;
 
-               google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                   return function() {
-                       infowindow.setContent(locations[i][0]);
-                       infowindow.open(map, marker);
-                   };
-               })(marker, i));
-           } // endfor
-       }
-</script>
-</body>
+                for (i = 0; i < locations.length; i++) {
+                    marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                        map: map
+                    });
+
+                    google.maps.event.addListener(marker, 'click', (function (marker, i) {
+                        return function () {
+                            infowindow.setContent(locations[i][0]);
+                            infowindow.open(map, marker);
+                        };
+                    })(marker, i));
+                } // endfor
+            }
+        </script>
+    </body>
 
 </html>
